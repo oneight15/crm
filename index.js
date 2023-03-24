9d75966eaeedc17f6204645e98072fc17b9c0af5
oneight15 <oneight1508@gmail.com>
2023-03-25 00:50:55 +0500

    feat: add solution lesson 2

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ console.log('Наименование товара: ', nameProduct);
 
 const sumProduct = countProduct * priceProduct;
 console.log('Общая сумма товара: ', sumProduct);
+
+const nameUserProduct = prompt('Введите наименование товара');
+const countUserProduct = +(prompt('Введите количество товара'));
+const categoryUserProduct = prompt('Введите категорию товара');
+const priceUserProduct = +(prompt('Введите цену товара'));
+const sumUserProduct = countUserProduct * priceUserProduct;
+
+console.log(typeof nameUserProduct)
+console.log(typeof countUserProduct)
+console.log(typeof categoryUserProduct)
+console.log(typeof priceUserProduct)
+console.log(`На складе ${countUserProduct} единицы товара ${nameUserProduct} на сумму ${sumUserProduct} деревянных`)
