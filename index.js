ee9701388bfd801c75d88c6dee05ef911f71cad4
oneight15 <oneight1508@gmail.com>
2023-03-24 17:37:15 +0500

    feat: add solution lesson 1

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const nameProduct = 'brick';
+const countProduct = 100;
+const categoryProduct = 'building';
+const priceProduct = 12;
+
+console.log('Наименование товара: ', nameProduct);
+
+const sumProduct = countProduct * priceProduct;
+console.log('Общая сумма товара: ', sumProduct);
