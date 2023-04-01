fd81dc4d6d8e31c4e77303d87e518f2fff692eff
oneight15 <oneight1508@gmail.com>
2023-04-01 14:29:39 +0500

    feat: add second var solution task02

--- js/lesson-5/task02.js
+++ js/lesson-5/task02.js
@@ -19,12 +19,24 @@ const isPrime = n => {
   return true;
 }
 
-console.log(isPrime(151));
-console.log(isPrime(911));
-console.log(isPrime(1));
-console.log(isPrime(2));
-console.log(isPrime(100));
-console.log(isPrime(77));
+// console.log(isPrime(151));
+// console.log(isPrime(911));
+// console.log(isPrime(1));
+// console.log(isPrime(2));
+// console.log(isPrime(100));
+// console.log(isPrime(77));
+
+
+// упрощенный вариант решения
+// const isPrime = n => {
+//   for (let i = 2; i <= Math.round(Math.sqrt(n)); i++) {
+//     if (!(n % i)) {
+//       return false;
+//     }
+//   }
+//   return n > 1;
+// }
+
 
 for (let i = -100, count = 0; i < 542; i++) {
   isPrime(i) && console.log(`${++count}) - ${i}.`)
