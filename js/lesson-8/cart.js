127e22bc0b422fc5f3336addb5eaee1592687d52
oneight15 <oneight1508@gmail.com>
2023-04-13 17:22:42 +0500

    init: start task

---
+++ js/lesson-8/cart.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const cart = {
+  items: [],
+  totalPrice: 0,
+  count: 0,
+  getTotalPrice() {
+    return this.totalPrice;
+  },
+  add() {
+
+  },
+  increaseCount() {
+
+  },
+  calculateItemPrice() {
+
+  },
+  clear() {
+
+  },
+  print() {
+
+  },
+}
+
+console.log(cart);
+console.log(cart.getTotalPrice());
