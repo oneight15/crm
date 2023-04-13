309c2f23c9db8290b94a01576ba8a000d1f5df25
oneight15 <oneight1508@gmail.com>
2023-04-13 17:04:50 +0500

    fix: correct solution

--- js/lesson-7/game01.js
+++ js/lesson-7/game01.js
@@ -1,25 +1,24 @@
 'use strict';
 
 const randomNumber = Math.ceil(Math.random() * 100);
-console.log(randomNumber);
+let tryToGuess = '';
 
 alert('Я загадал число от 1 до 100, попробуй угадать');
 
-while (true) {
-  let tryToGuess = prompt('Введи число');
-
-  if (tryToGuess === null) {
-    alert('Сыграем в другой раз!');
-    break;
-  } else if (isNaN(tryToGuess)) {
-    alert('Введи число!');
-  }
+while (tryToGuess !== null && +tryToGuess !== randomNumber) {
+  tryToGuess = prompt('Введи число');
 
   switch (true) {
-    case (+tryToGuess === randomNumber):
-      alert('Правильно, ты угадал!');
+    case tryToGuess === null:
+      alert('Сыграем в другой раз!');
       break;
 
+    case isNaN(tryToGuess):
+    case +tryToGuess < 1:
+    case +tryToGuess > 100:
+      alert('Введи число от 1 до 100!');
+      break;
+      
     case (+tryToGuess > randomNumber):
       alert('Загаданное число меньше!');
       break;
@@ -29,14 +28,9 @@ while (true) {
       break;
       
     default:
-      alert('Введи число!');
+      alert('Правильно, ты угадал!');
       break;
   }
-
-  if (+tryToGuess === randomNumber) {
-    break;
-  }
-
 }
 
 
--- js/lesson-7/task03.js
+++ js/lesson-7/task03.js
@@ -2,31 +2,20 @@
 
 const getRandomArr = (x, n = 0, m = 0, str) => {
   const randomArr = [];
+  const rules = ['even', 'odd'].indexOf(str.trim().toLowerCase());
   const max = Math.max(n, m);
   const min = Math.min(n, m);
 
   while (x > randomArr.length) {
     const randomItem = Math.floor(Math.random() * (max - min + 1) + min);
 
-    switch (str) {
-      case 'even':
-        if (randomItem % 2 === 0) {
-          randomArr.push(randomItem);
-        }
-        break;
-
-      case 'odd':
-        if (randomItem % 2 !== 0) {
-          randomArr.push(randomItem);
-        }
-        break;
-
-      default:
-        randomArr.push(randomItem);
+    if (rules === -1 || rules === Math.abs(randomItem % 2)) {
+      randomArr.push(randomItem);
     }
   }
+
   return randomArr;
 }
 
-console.log(getRandomArr(99, -10, 10, 'odd'));
+console.log(getRandomArr(99, -11, 11, '123'));
 
