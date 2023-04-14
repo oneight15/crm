ad04399358bb90228bdf59c82e75df4c66e01620
oneight15 <oneight1508@gmail.com>
2023-04-15 00:32:31 +0500

    feat: add obj method

--- js/lesson-8/cart.js
+++ js/lesson-8/cart.js
@@ -13,13 +13,15 @@ const cart = {
     item.price = itemPrice;
     item.count = itemCount;
     this.items.push(item);
-
+    this.calculateItemPrice();
+    this.count += item[Object.keys(item)[2]];
   },
   increaseCount(n) {
     this.count += n;
   },
   calculateItemPrice() {
-
+    const priceValue = this.items.flatMap(item => item[Object.keys(item)[1]] * item[Object.keys(item)[2]])
+    this.totalPrice = priceValue.reduce((sum, elem) => sum + elem, 0);
   },
   clear() {
     this.items = [];
@@ -27,6 +29,33 @@ const cart = {
     this.count = 0;
   },
   print() {
-
+    console.log(JSON.stringify(this.items));
+    console.log(`Общая стоимость корзины: ${this.getTotalPrice()}`);
   },
 }
+
+console.log('общая сумма корзины:', cart.getTotalPrice());
+console.log('общее количество товаров:', cart.count);
+console.log('____________________________________________');
+
+cart.add('мышка', 700, 5);
+console.log('общая сумма корзины:', cart.getTotalPrice());
+console.log('общее количество товаров:', cart.count);
+console.log('____________________________________________');
+
+cart.add('клавиатура', 600, 7);
+console.log('общая сумма корзины:', cart.getTotalPrice());
+console.log('общее количество товаров:', cart.count);
+console.log('____________________________________________');
+
+cart.add('монитор', 44000, 2);
+console.log('общая сумма корзины:', cart.getTotalPrice());
+console.log('общее количество товаров:', cart.count);
+console.log('____________________________________________');
+
+cart.add('ноутбук', 17000, 3);
+console.log('общая сумма корзины:', cart.getTotalPrice());
+console.log('общее количество товаров:', cart.count);
+console.log('____________________________________________');
+
+cart.print();
