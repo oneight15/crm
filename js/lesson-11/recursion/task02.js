a75ae6d10524909a20b59562b6afdf16d515b4e3
oneight15 <oneight1508@gmail.com>
2023-04-19 18:05:17 +0500

    feat: add solution recursion task02

--- js/lesson-11/recursion/task02.js
+++ js/lesson-11/recursion/task02.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const getSomeArr = arr => {
+  arr.push(Math.round(Math.random() * 10));
+  let sum = arr.reduce((item, sum) => item + sum, 0);
+
+  if (sum > 50) {
+    return arr;
+  }
+  
+  return getSomeArr(arr);
+}
+
+const epmtyArr = [];
+const fibo = [0, 1, 1, 2, 3, 5, 8, 13];
+
+console.log(getSomeArr(epmtyArr));
+console.log(getSomeArr(fibo));
