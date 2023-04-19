32af838eba2da805d33f1c624aba4850bfb62a40
oneight15 <oneight1508@gmail.com>
2023-04-19 15:46:29 +0500

    feat: add solution recursion task01

---
+++ js/lesson-11/recursion/task01.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const randomNumber = Math.ceil(Math.random() * 100);
+let tryToGuess = '';
+
+alert('Я загадал число от 1 до 100, попробуй угадать');
+
+const game = tryToGuess => {
+  tryToGuess = prompt('Введи число');
+
+  if (tryToGuess === null) {
+    alert('Сыграем в другой раз!');
+    return;
+  }
+
+  switch (true) {
+    case isNaN(tryToGuess):
+    case +tryToGuess < 1:
+    case +tryToGuess > 100:
+      alert('Введи число от 1 до 100!');
+      break;
+      
+    case (+tryToGuess > randomNumber):
+      alert('Загаданное число меньше!');
+      break;
+      
+    case (+tryToGuess < randomNumber):
+      alert('Загаданное число больше!');
+      break;
+      
+    default:
+      alert('Правильно, ты угадал!');
+      return;
+  }
+
+  return game();
+}
+
+game();
